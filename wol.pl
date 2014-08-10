cada78b56a339a8ff6456ac8b160263d68ebb382
Bruce Blank <Torsten.Blank@gmail.com>
2014-08-10 19:19:22 +0200

    output added

--- wol.pl
+++ wol.pl
@@ -5,9 +5,9 @@
 $domain = "local";
 
 (@ARGV == 1) || die "exactly one argument required.";
-($macadr = $hosts{$ARGV[0]}) || die "wrong hostname \"${ARGV[0]}\"";
+$hostname = $ARGV[0];
+($macadr = $hosts{$hostname}) || die "wrong hostname \"${ARGV[0]}\"";
 
 # here we have an existing MAC / Hostname -> wake it
+print "waking host \"${hostname}\" with MAC \"${macadr}\":\n\n";
 system("sudo etherwake -D ${macadr}");
-
-
